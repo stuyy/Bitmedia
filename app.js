a1e24c317b8b4aa1e3d1618ba428e22234a5c9d8
Anson <ansfoong@gmail.com>
2019-11-04 19:33:32 -0500

    Added SessionStore

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalAuth = require('./strategies/local');
-
+const SessionStore = require('express-session-sequelize')(session.Store);
+const database = require('./database/database');
 app.use(cookieParser('keyboard cat'));
 
 app.use(flash());
@@ -18,16 +19,19 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 360000
-    }
+    },
+    store: new SessionStore({ db: database })
 }));
 
 app.use(passport.initialize()); 
 app.use(passport.session());
 
 const authRoute = require('./routes/auth');
+const dashboardRoute = require('./routes/dashboard');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use('/', authRoute);
+app.use('/dashboard', dashboardRoute);
 
 app.listen(3000, console.log(`Listening on port 3000.`));
