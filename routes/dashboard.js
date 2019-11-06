2f089cbba1fe65640c5a94a42becd7d5d637586b
Anson <ansfoong@gmail.com>
2019-11-06 09:36:17 -0500

    Added check to user session.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    let user = req.user.dataValues;
-    res.render('dashboard', { title: 'Dashboard', firstName: user.firstName, lastName: user.lastName, email: user.email, createdAt: user.createdAt });
+    if(req.user) {
+        let user = req.user.dataValues;
+        res.render('dashboard', { title: 'Dashboard', firstName: user.firstName, lastName: user.lastName, email: user.email, createdAt: user.createdAt });
+    }
+    else {
+        res.status(403);
+        res.redirect('/register')
+    }
+    
 })
 
 module.exports = router;
