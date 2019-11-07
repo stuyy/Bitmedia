6f89dab8785b2b4e229a82413f79770bf00974ac
Anson <ansfoong@gmail.com>
2019-11-07 13:38:26 -0500

    Fetch last status and send it to the front end.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,15 +1,26 @@
 const router = require('express').Router();
+const Status = require('../models/StatusUpdate');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     if(req.user) {
         let user = req.user.dataValues;
-        res.render('dashboard', { title: 'Dashboard', firstName: user.firstName, lastName: user.lastName, email: user.email, createdAt: user.createdAt });
+        let status = await Status.findAll({ where: {
+            authorId: user.email
+        }, order: [['createdAt', 'DESC']]})
+        .catch(err => console.log(err));
+        let lastStatus;
+        if(status.length !== 0)
+            lastStatus = status[0].dataValues;
+        else
+            lastStatus = null;
+        
+        res.render('dashboard', { title: 'Dashboard', firstName: user.firstName, lastName: user.lastName, email: user.email, createdAt: user.createdAt, lastStatus: lastStatus });
     }
     else {
         res.status(403);
         res.redirect('/register')
     }
     
-})
+});
 
-module.exports = router;
+module.exports = router;    
