bacdc49ab6e40ee8bda72982f866f8a11af191b1
Anson <ansfoong@gmail.com>
2019-11-07 12:46:55 -0500

    Added resave: false to session because first login after server restart redirects to register.

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const passport = require('passport');
 const LocalAuth = require('./strategies/local');
 const SessionStore = require('express-session-sequelize')(session.Store);
 const database = require('./database/database');
+const PORT = process.env.PORT || 3506;
+const morgan = require('morgan')
+app.use(morgan('tiny'))
 app.use(cookieParser('keyboard cat'));
 
 app.use(flash());
@@ -15,8 +18,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(session({
-    secret: 'keyboard cat',
+    secret: 'some string',
     saveUninitialized: false,
+    resave: false,
     cookie: {
         maxAge: 360000
     },
@@ -28,10 +32,15 @@ app.use(passport.session());
 
 const authRoute = require('./routes/auth');
 const dashboardRoute = require('./routes/dashboard');
+const userRoute = require('./routes/user');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
 app.use('/', authRoute);
 app.use('/dashboard', dashboardRoute);
+app.use('/user', userRoute);
 
-app.listen(process.env.PORT, console.log(`Listening on port 3000.`));
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
+});
