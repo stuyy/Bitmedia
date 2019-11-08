188085f5d58c87f26bf69a4b5940e538f6b40af7
Anson <ansfoong@gmail.com>
2019-11-07 21:30:08 -0500

    Moved createNewTask() to requests.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,33 +8,6 @@ function closeDialogTask() {
     dialog.close();
 }
 
-function createNewTask() {
-    let title = document.getElementById('taskTitle').value;
-    let description = document.getElementById('taskDescription').value;
-    let errorDiv = document.getElementById('errors');
-    removeChildrenNodes(errorDiv);
-    try {
-        if(title.length < 10) {
-            let errorMsg = 'Title cannot be less than 10 characters!';
-            let alertElement = generateBootstrapAlert('danger', errorMsg);
-            errorDiv.appendChild(alertElement);
-            setTimeout(() => errorDiv.removeChild(alertElement), 5000);
-            throw new Error("Title cannot be less than 10 characters")
-        }
-        if(description.length < 10) {
-            let errorMsg = 'Invalid Description';
-            let alertElement = generateBootstrapAlert('danger', errorMsg);
-            errorDiv.appendChild(alertElement);
-            setTimeout(() => errorDiv.removeChild(alertElement), 5000);
-            throw new Error("Title cannot be less than 10 characters")
-        }
-        console.log("Success?")
-    } 
-    catch(e) {
-        console.log(e);
-    }
-}
-
 function removeChildrenNodes(elementNode) {
     let m = Array.from(elementNode.childNodes)
     m.forEach(m => {
