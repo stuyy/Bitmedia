3aebf26c8aa3205aed3a0eed483afd32e5477f37
Anson <ansfoong@gmail.com>
2019-11-07 21:30:21 -0500

    Return Status and Task to front end.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Status = require('../models/StatusUpdate');
+const Task = require('../models/Task');
 
 router.get('/', async (req, res) => {
     if(req.user) {
@@ -8,19 +9,27 @@ router.get('/', async (req, res) => {
             authorId: user.email
         }, order: [['createdAt', 'DESC']]})
         .catch(err => console.log(err));
-        let lastStatus;
-        if(status.length !== 0)
-            lastStatus = status[0].dataValues;
-        else
-            lastStatus = null;
-        
-        res.render('dashboard', { title: 'Dashboard', firstName: user.firstName, lastName: user.lastName, email: user.email, createdAt: user.createdAt, lastStatus: lastStatus });
+
+        let tasks  = await Task.findAll({
+            where: { authorId: user.email },
+            order: [['createdAt', 'DESC']]
+        }).catch(err => console.log(err));
+
+        let lastStatus = status.slice(0, 1).map(s => s.dataValues);
+
+        lastStatus = lastStatus.length === 0 ? null : lastStatus.shift();
+
+        let recentTasks = tasks.splice(0,3).map(t => t.dataValues);
+        if(recentTasks.length === 0)
+            recentTasks = null;
+
+        console.log(recentTasks)
+        res.render('dashboard', { title: 'Dashboard', firstName: user.firstName, lastName: user.lastName, email: user.email, createdAt: user.createdAt, lastStatus: lastStatus, recentTasks: recentTasks });
     }
     else {
         res.status(403);
         res.redirect('/register')
     }
-    
 });
 
 module.exports = router;    
