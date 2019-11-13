c6e46ddaa3307ba73aaaa7ea984d6aa6c2135344
Anson <ansfoong@gmail.com>
2019-11-12 22:39:03 -0500

    Decrypt email in verifyCode function in account.js

--- routes/account.js
+++ routes/account.js
@@ -1,15 +1,24 @@
 const router = require('express').Router();
-const crypto = require('crypto');
-const EventEmitter = require('events').EventEmitter;
+const Encrypter = require('../utils/Encrypter');
+const ServerHandler = require('../utils/ServerEvents');
 // Need initialization vector for randomness.
 // Need a key
 
 const codes = new Map();
 
-function verifyCode(req, res, next) {
-    console.log(req.resetToken);
+/**
+ * Middleware function used to verify if the query parameter 'code' is
+ * valid. Every code will decrypt to an e-mail address that is in the
+ * Database. We need to decrypt every code first
+ */
+async function verifyCode(req, res, next) {
     let { code } = req.query;
+    let decryptedEmail = await Encrypter.decrypt(code)
+        .catch(err => console.log(err));
+    console.log(decryptedEmail)
     if(codes.has(code)) {
+        console.log("Code is valid, and is in the map..");
+        console.log(codes);
         next();
     }
     else {
@@ -25,6 +34,16 @@ router.get('/password/reset/', verifyCode, (req, res) => {
     res.render('routes/resetpw', {
         title: 'Reset Your Password'
     });
+});
+
+ServerHandler.on('onEmailSend', email => {
+    codes.set(email, true);
+    console.log("Hello?????");
+    console.log(codes);
+    setTimeout(() => {
+        codes.delete(email);
+        console.log("Removed code.");
+    }, 5 * 60 * 1000); // 10 Minutes.
 });
 
 module.exports = router;
